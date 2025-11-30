854ae95f284508287115b5f113c30f3467f7f83b
Shubhang <abhayshubhang@gmail.com>
2025-11-30 17:15:36 +0530

    added empty Dashboard component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {ToastContainer} from 'react-toastify'
 import Navbar from './components/common/Navbar/Navbar'
+import Dashboard from './pages/customer/Dashboard'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Dashboard />
       <ToastContainer />
     </>
   )
