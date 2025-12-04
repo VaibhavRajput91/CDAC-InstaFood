070ca8c6253dc5f157fb667021049914f6baf930
Shubhang <abhayshubhang@gmail.com>
2025-12-04 23:00:18 +0530

    just changed the Route for customer dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navbar />} />
         <Route path="/customer/menu" element={<CustomerMenu />} />
-        <Route path="/customer/order-summary" element={<CustomerOrderSummary />} />
+        <Route path="/customer/dashboard" element={<Dashboard />} />
         <Route path="/admin/*" element={<Admin />} />
 
       </Routes>
