51578e744f4ea5b8e8620c2e53c546d8aa47dbdb
Vaibhav Rajput <vaibhavrajputhk2003@gmail.com>
2025-12-06 23:08:20 +0530

    d

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,22 @@ import {ToastContainer} from 'react-toastify'
 import Profile from './pages/delivery/Profile/Profile'
 import EditProfile from './pages/delivery/EditProfile/EditProfile'
 import Navbar from './components/common/Navbar/Navbar'
-import Dashboard from './pages/customer/Dashboard/Dashboard'
+// import Dashboard from './pages/customer/Dashboard/Dashboard'
 import { Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/delivery/Dashboard/Dashboard';
+// import Dashboard from './components/admin/Dashboard/Dashboard';
 
 function App() {
 
   return (
     <>
-      <Routes>
-      <Route path="/" element={<Navbar />} />
-      <Route path="/customer/dashboard" element={<Dashboard />} />
+      {/* <Routes> */}
+      {/* <Route path="/" element={<Navbar />} />
+      <Route path="/customer/dashboard" element={<Dashboard />} /> */}
+      <Profile />
+      <Dashboard />
 
-      </Routes>
+      {/* </Routes> */}
       <ToastContainer />
     </>
   )
