7f7a4abdf47cbade70dc73fced4b5aed36a781e9
ErDigvijay407 <er.digvijay407@gmail.com>
2025-12-08 21:42:33 +0530

    Custom-navbar-in-admin

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Link, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "../../components/common/Navbar/Navbar";
 import Dashboard from "../../components/admin/Dashboard/Dashboard";
 import Statistics from "../../components/admin/Statistics/Statistics";
@@ -15,7 +15,31 @@ import Profile from "../../components/admin/Profile/Profile"
 function Admin() {
     return (
         <div>
-            <Navbar />
+            <nav className="w-full bg-red-600 shadow-sm sticky top-0 z-50">
+                <div className="max-w-7xl mx-auto px-4 flex items-center justify-center py-4 md:justify-between md:py-3">
+
+                    {/* CENTER LOGO ON MOBILE */}
+                    <div className="text-2xl m-px font-semibold text-white absolute left-1/2 -translate-x-1/2 md:static md:translate-x-0">
+                        InstaFood
+                    </div>
+
+                    {/* Desktop Menu */}
+                    {/* We can add more links accordingly or maybe use conditional rendering to display different navbar for different user roles */}
+                    <ul className="hidden md:flex space-x-8 text-white">
+                        <li><Link to="/admin/dashboard" className="hover:text-gray-900">Home</Link></li>
+                        <li><Link to="/admin/statistics" className="hover:text-gray-900">Statistics</Link></li>
+                        <li><Link to="/admin/approvals" className="hover:text-gray-900">Approvals</Link></li>
+                        <li><Link to="/admin/profile" className="hover:text-gray-900">Profile</Link></li>
+                    </ul>
+
+                    {/* Desktop Login Button */}
+                    <button className="hidden md:block bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded-lg">
+                        Login
+                    </button>
+                </div>
+            </nav>
+
+
             <Routes>
                 <Route
                     path="/"
