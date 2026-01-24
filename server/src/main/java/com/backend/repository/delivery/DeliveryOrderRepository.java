97a9380616a0bbb3c82fc41b38fb777375cb99e4
Vaibhav Rajput <vaibhavrajputhk2003@gmail.com>
2026-01-24 16:42:04 +0530

    orderdto and orderdetailsDTO list

--- server/src/main/java/com/backend/repository/delivery/DeliveryOrderRepository.java
+++ server/src/main/java/com/backend/repository/delivery/DeliveryOrderRepository.java
@@ -1,5 +1,6 @@
 package com.backend.repository.delivery;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.backend.entity.Order;
 
 public interface DeliveryOrderRepository extends JpaRepository<Order, Long> {
-	public List<Order> findByDeliveryPartnerId(long deliveryPartnerId);
+	public List<Order> findByDeliveryPartnerIdAndCreatedOnBetween(Long deliveryPartnerId, LocalDateTime start, LocalDateTime end);
+	
+	public List<Order> findByDeliveryPartnerIdOrderByCreatedOnDesc(Long deliveryPartnerId);
 }
