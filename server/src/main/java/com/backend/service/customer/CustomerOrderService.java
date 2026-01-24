8290fef91ae40cc5100f8c647fb245cabcab9496
Shubhang <abhayshubhang@gmail.com>
2026-01-25 00:14:14 +0530

    fixed earlier mistake of listing all orders of a customer - now it shows all orders of a customer rather than just one

--- server/src/main/java/com/backend/service/customer/CustomerOrderService.java
+++ server/src/main/java/com/backend/service/customer/CustomerOrderService.java
@@ -1,10 +1,12 @@
 package com.backend.service.customer;
 
+import java.util.List;
+
 import com.backend.dto.customer.OrdersByCustomerDTO;
 
 public interface CustomerOrderService {
 	
-	OrdersByCustomerDTO getAllOrdersByCustomer(Long customerId); //METHOD TO GET ALL ORDERS BY A CUSTOMER
+	List<OrdersByCustomerDTO> getAllOrdersByCustomer(Long customerId); //METHOD TO GET ALL ORDERS BY A CUSTOMER
 	
 
 }
