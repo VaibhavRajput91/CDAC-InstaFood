6cf4615d4b22cbce1a4b747d432a433ea8122305
KiranPote321 <kiranpote2013@gmail.com>
2026-01-25 09:48:58 +0530

    completed admin api of restaurant applications

--- server/src/main/java/com/backend/service/admin/AdminServiceImpl.java
+++ server/src/main/java/com/backend/service/admin/AdminServiceImpl.java
@@ -1,5 +1,8 @@
 package com.backend.service.admin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -7,9 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.backend.dto.admin.AdminProfileDTO;
+import com.backend.dto.admin.RestaurantApplicationsDTO;
+import com.backend.entity.AvailabilityStatus;
+import com.backend.entity.Restaurant;
 import com.backend.entity.User;
 import com.backend.entity.UserRole;
 import com.backend.repository.admin.AdminProfileRepository;
+import com.backend.repository.admin.RestaurantApprovalRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +27,7 @@ public class AdminServiceImpl implements AdminService{
 
 	private final AdminProfileRepository adminProfileRepository;
 	
+	private final RestaurantApprovalRepository restaurantApprovalRepository;
 	private final ModelMapper modelMapper;
 
 	@Override
@@ -31,5 +39,25 @@ public class AdminServiceImpl implements AdminService{
 		System.out.println(user);
 		AdminProfileDTO adminProfile = modelMapper.map(user, AdminProfileDTO.class);
 		return adminProfile;
+	}
+
+	@Override
+	public List<RestaurantApplicationsDTO> getPendingRestaurantApplications() {
+		
+		List<Restaurant> restaurants =
+				restaurantApprovalRepository.findByStatus(AvailabilityStatus.INACTIVE);
+		
+        // Convert Entity → DTO (USING FOR LOOP)
+        List<RestaurantApplicationsDTO> dtoList = new ArrayList<>();
+
+        for (Restaurant restaurant : restaurants) {
+            RestaurantApplicationsDTO restaurantdto = new RestaurantApplicationsDTO();
+            restaurantdto.setId(restaurant.getId());
+            restaurantdto.setRestaurantName(restaurant.getRestaurantName());
+
+            dtoList.add(restaurantdto);
+        }
+
+        return dtoList;
 	}
 }
